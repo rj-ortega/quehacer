1fa548599956369a31fc9e8004a9545ad70953fb
Ricardo Ortega <rikrdo.ortega@gmail.com>
2024-02-25 13:03:50 -0700

    added edit functionality, added filter tasks

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,23 @@ import FilterButton from "./components/FilterButton";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
-function App(props: {
-  tasks: Array<{ name: string; id: string; completed: boolean }>;
-}) {
+type Task = { name: string; id: string; completed: boolean };
+type Props = { tasks: Task[] };
+type FilterMapKeys = "All" | "Active" | "Completed";
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task: Task) => !task.completed,
+  Completed: (task: Task) => task.completed,
+};
+
+// type FilterMapKeys = keyof typeof FILTER_MAP;
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+const App = (props: Props) => {
   const [tasks, setTasks] = useState(props.tasks);
-  function toggleTaskCompleted(id: string) {
+  const [filter, setFilter] = useState<FilterMapKeys>("All");
+  const toggleTaskCompleted = (id: string) => {
     const updatedTasks = tasks.map((task) => {
       if (id === task.id) {
         return { ...task, completed: !task.completed };
@@ -16,36 +28,52 @@ function App(props: {
       return task;
     });
     setTasks(updatedTasks);
-  }
-  function deleteTask(id: string) {
+  };
+  const deleteTask = (id: string) => {
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
-  }
-  const taskList = tasks?.map((task) => (
-    <Todo
-      id={task.id}
-      key={task.id}
-      name={task.name}
-      completed={task.completed}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
+  };
+  const editTask = (id: string, newName: string) => {
+    const editedTaskList = tasks.map((task) => {
+      if (id == task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  };
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        key={task.id}
+        name={task.name}
+        completed={task.completed}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
-  function addTask(name: string) {
+  const addTask = (name: string) => {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
-  }
+  };
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>Que Haceres</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
@@ -56,6 +84,6 @@ function App(props: {
       </ul>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/FilterButton.tsx
+++ src/components/FilterButton.tsx
@@ -1,11 +1,23 @@
-function FilterButton() {
+type Props = {
+  key: string;
+  name: string;
+  isPressed: boolean;
+  setFilter: Function;
+};
+
+const FilterButton = (props: Props) => {
   return (
-    <button type="button" className="btn toggle-btn" aria-pressed="true">
+    <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.name)}
+    >
       <span className="visually-hidden">Show </span>
-      <span>Nothing </span>
+      <span>{props.name}</span>
       <span className="visually-hidden"> tasks</span>
     </button>
   );
-}
+};
 
 export default FilterButton;
